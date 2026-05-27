47c90ffd2c2d9e239ce2e486ed9fdc29abc655a8
BalArto <artur.balon@gmail.com>
2026-05-27 14:50:44 +0200

    poprawki w skrypcie powershell + zip

--- AssecoTools/ZippedRelease/ZippReleaseVersion.ps1
+++ AssecoTools/ZippedRelease/ZippReleaseVersion.ps1
@@ -4,22 +4,7 @@
     Kompresuje wersjê Release do dwóch lokalizacji 
 #>
 
-$SharePath = "C:\vm_share\AssecoTools.zip"
-If(Test-Path $SharePath)
-{
-    Remove-Item $sharePath
-    }
-
-$compress = @{
-Path = "C:\Users\Artur.Balon\Documents\GitLocal\AssecoTools\AssecoToolsApplication\AssecoTools\bin\x64\Release\*.dll",
-       "C:\Users\Artur.Balon\Documents\GitLocal\AssecoTools\AssecoToolsApplication\AssecoTools\bin\x64\Release\*.config",
-       "C:\Users\Artur.Balon\Documents\GitLocal\AssecoTools\AssecoToolsApplication\AssecoTools\bin\x64\Release\AssecoTools.exe" 
-CompressionLevel = "Fastest"
-DestinationPath = "C:\vm_share\AssecoTools.zip"
-}
-Compress-Archive @compress
-
-$ReleasePath = "C:\Users\Artur.Balon\Documents\GitLocal\AssecoTools\AssecoToolsApplication\AssecoTools\ZippedRelease\AssecoTools.zip"
+$ReleasePath = "C:\Users\Art\Dokumenty\Visual Studio 2026\Projects\AssecoTools\AssecoToolsApplication\AssecoTools\ZippedRelease\AssecoTools.zip"
 
 if(Test-Path $ReleasePath)
 {
@@ -27,10 +12,10 @@ if(Test-Path $ReleasePath)
     }
 
 $compress2 = @{
-Path = "C:\Users\Artur.Balon\Documents\GitLocal\AssecoTools\AssecoToolsApplication\AssecoTools\bin\x64\Release\*.dll",
-       "C:\Users\Artur.Balon\Documents\GitLocal\AssecoTools\AssecoToolsApplication\AssecoTools\bin\x64\Release\*.config",
-       "C:\Users\Artur.Balon\Documents\GitLocal\AssecoTools\AssecoToolsApplication\AssecoTools\bin\x64\Release\AssecoTools.exe" 
+Path = "C:\Users\Art\Dokumenty\Visual Studio 2026\Projects\AssecoTools\AssecoToolsApplication\AssecoTools\bin\x64\Release\*.dll",
+       "C:\Users\Art\Dokumenty\Visual Studio 2026\Projects\AssecoTools\AssecoToolsApplication\AssecoTools\bin\x64\Release\*.config",
+       "C:\Users\Art\Dokumenty\Visual Studio 2026\Projects\AssecoTools\AssecoToolsApplication\AssecoTools\bin\x64\Release\AssecoTools.exe" 
 CompressionLevel = "Fastest"
-DestinationPath = "C:\Users\Artur.Balon\Documents\GitLocal\AssecoTools\AssecoToolsApplication\AssecoTools\ZippedRelease\AssecoTools.zip"
+DestinationPath = "C:\Users\Art\Dokumenty\Visual Studio 2026\Projects\AssecoTools\AssecoToolsApplication\AssecoTools\ZippedRelease\AssecoTools.zip"
 }
 Compress-Archive @compress2
